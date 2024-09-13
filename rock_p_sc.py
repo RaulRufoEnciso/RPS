9b34a2c8fcfce23c2a20c0afbc0aeb6ccee2dbab
RaulRufoEnciso <rrufo03@gmail.com>
2024-09-13 13:30:14 +0200

    funciones establecidas y codigo sintetizado

--- rock_p_sc.py
+++ rock_p_sc.py
@@ -1,5 +1,51 @@
 import random
-rock = '''
+import functools
+import time
+import datetime
+
+def registrar_tiempo(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+def contador_juegos(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+def registrar_fecha_hora(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+def contador_victorias(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+def contador_derrotas(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+def contador_empates(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+def limite_de_partidas(func):
+    @functools.wraps(funcs)
+    def wraper(*args,**kwargs):
+        return
+    return
+
+piedra = '''
     _______
 ---'   ____)
       (_____)
@@ -8,7 +54,7 @@ rock = '''
 ---.__(___)
 '''
 
-paper = '''
+papel = '''
     _______
 ---'   ____)____
           ______)
@@ -17,7 +63,7 @@ paper = '''
 ---.__________)
 '''
 
-scissors = '''
+tijeras = '''
     _______
 ---'   ____)____
           ______)
@@ -26,26 +72,20 @@ scissors = '''
 ---.__(___)
 '''
 
-game_images =[rock, paper, scissors]
+game_img =[piedra, papel, tijeras]
+ask = input("Quieres jugar? ").lower()
+while ask != "no":
 
-user_pick = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if (user_pick >= 3 or user_pick< 0):
-    print("You typed an invalid number, you lose!")
-else:
-    print(game_images[user_pick])
-
-    computer_pick = random.randint(0,2)
-    print("Computer chose:")
-    print(game_images[computer_pick])
-
-    if user_pick == 0 and computer_pick == 2:
-        print("You win!")
-    elif computer_pick == 0 and user_pick == 2:
-        print("You lose!")
-    elif computer_pick > user_pick:
-        print("You lose")
-    elif user_pick > computer_pick:
-        print("You win")
-    elif computer_pick == user_pick:
-        print("It's a draw")
+    opcion = int(input("Que elijes? Piedra (0), Papel (1), Tijeras (2).\n"))
+    if (opcion >= 3 or opcion< 0):
+        print("Elejiste un numero erroneo, pierdes!")
+    else:
+        print(game_img[opcion])
+        computer_pick = random.randint(0,2)
+        print("Eleccion del contrincante:"); print(game_img[computer_pick])
+        if opcion == 0 and computer_pick == 2 or opcion > computer_pick: print("Ganas!")
+        elif computer_pick == 0 and opcion == 2 or computer_pick > opcion: print("Pierdes")
+        elif computer_pick == opcion: print("Empate")
+    ask = input("Quieres seguir jugando? ").lower()
+        
 
